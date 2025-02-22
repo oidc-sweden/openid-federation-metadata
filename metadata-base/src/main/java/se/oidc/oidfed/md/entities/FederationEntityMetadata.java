dd80ae2fb8beb0b3a08b2ca778623974c99519ce
Stefan Santesson <stefan@aaa-sec.com>
2025-02-22 03:37:31 +0100

    Update federation metadata and endpoint definitions
    Improved federation metadata structure by adding missing endpoint definitions and related documentation. Simplified and corrected metadata model fields, removed deprecated attributes, and updated version to 1.1.0 in the project configuration to reflect these enhancements.

--- metadata-base/src/main/java/se/oidc/oidfed/md/entities/FederationEntityMetadata.java
+++ metadata-base/src/main/java/se/oidc/oidfed/md/entities/FederationEntityMetadata.java
@@ -39,45 +39,93 @@ public class FederationEntityMetadata extends AbstractOidcFedMetadata {
     this.addLanguageParametersTags(List.of());
   }
 
+  /**
+   * OPTIONAL. The fetch endpoint described in Section 8.1. This URL MUST use the https scheme and MAY contain port,
+   * path, and query parameter components; it MUST NOT contain a fragment component. Intermediate Entities and Trust
+   * Anchors MUST publish a federation_fetch_endpoint. Leaf Entities MUST NOT.
+   */
   @JsonProperty("federation_fetch_endpoint")
   @Getter
   @Setter
   private String federationFetchEndpoint;
 
+  /**
+   * OPTIONAL. The list endpoint described in Section 8.2. This URL MUST use the https scheme and MAY contain port,
+   * path, and query parameter components; it MUST NOT contain a fragment component. Intermediate Entities and Trust
+   * Anchors MUST publish a federation_list_endpoint. Leaf Entities MUST NOT.
+   */
   @JsonProperty("federation_list_endpoint")
   @Getter
   @Setter
   private String federationListEndpoint;
 
+  /**
+   * OPTIONAL. The resolve endpoint described in Section 8.3. This URL MUST use the https scheme and MAY contain port,
+   * path, and query parameter components; it MUST NOT contain a fragment component. Any federation Entity MAY publish a
+   * federation_resolve_endpoint.
+   */
   @JsonProperty("federation_resolve_endpoint")
   @Getter
   @Setter
   private String federationResolveEndpoint;
 
+  /**
+   * OPTIONAL. The Trust Mark status endpoint described in Section 8.4. Trust Mark Issuers SHOULD publish a
+   * federation_trust_mark_status_endpoint. This URL MUST use the https scheme and MAY contain port, path, and query
+   * parameter components; it MUST NOT contain a fragment component.
+   */
   @JsonProperty("federation_trust_mark_status_endpoint")
   @Getter
   @Setter
   private String federationTrustMarkStatusEndpoint;
 
+  /**
+   * OPTIONAL. The endpoint described in Section 8.5. This URL MUST use the https scheme and MAY contain port, path, and
+   * query parameter components; it MUST NOT contain a fragment component. Trust Mark Issuers MAY publish a
+   * federation_trust_mark_list_endpoint.
+   */
   @JsonProperty("federation_trust_mark_list_endpoint")
   @Getter
   @Setter
   private String federationTrustMarkListEndpoint;
 
+  /**
+   * OPTIONAL. The endpoint described in Section 8.6. This URL MUST use the https scheme and MAY contain port, path, and
+   * query parameter components; it MUST NOT contain a fragment component. Trust Mark Issuers MAY publish a
+   * federation_trust_mark_endpoint.
+   */
   @JsonProperty("federation_trust_mark_endpoint")
   @Getter
   @Setter
   private String federationTrustMarkEndpoint;
 
+  /**
+   * OPTIONAL. The endpoint described in Section 8.7. This URL MUST use the https scheme and MAY contain port, path, and
+   * query parameter components; it MUST NOT contain a fragment component. All Federation Entities MAY publish a
+   * federation_historical_keys_endpoint.
+   */
   @JsonProperty("federation_historical_keys_endpoint")
   @Getter
   @Setter
   private String federationHistoricalKeysEndpoint;
 
+  /**
+   * The `federationDiscoveryEndpoint` field represents the URL endpoint for the discovery of federation metadata. It is
+   * used in the context of OIDC federation to locate and retrieve metadata associated with a federation entity.
+   */
   @JsonProperty("federation_discovery_endpoint")
   @Getter
   @Setter
   private String federationDiscoveryEndpoint;
+
+  /**
+   * OPTIONAL. JSON array containing a list of the supported JWS [RFC7515] algorithms (alg values) for signing the JWT
+   * [RFC7519] used for private_key_jwt when authenticating to federation endpoints, as described in Section 8.8. No
+   * default algorithms are implied if this entry is omitted. Servers SHOULD support RS256. The value none MUST NOT be
+   * used.
+   */
+  @JsonProperty("endpoint_auth_signing_alg_values_supported")
+  private List<String> endpointAuthSigningAlgValuesSupported;
 
   /** {@inheritDoc} */
   @Override
@@ -153,6 +201,12 @@ public class FederationEntityMetadata extends AbstractOidcFedMetadata {
 
     public FederationEntityMetadataBuilder federationDiscoveryEndpoint(final String federationDiscoveryEndpoint) {
       this.metadata.federationDiscoveryEndpoint = federationDiscoveryEndpoint;
+      return this;
+    }
+
+    public FederationEntityMetadataBuilder endpointAuthSigningAlgValuesSupported(
+        final List<String> endpointAuthSigningAlgValuesSupported) {
+      this.metadata.endpointAuthSigningAlgValuesSupported = endpointAuthSigningAlgValuesSupported;
       return this;
     }
 
